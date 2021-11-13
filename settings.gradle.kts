5fcd13d87c3f75ff7766792fb7a6c50895d2da36
koga yushi <youshe.old.pleasure@gmail.com>
2021-11-13 11:48:02 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-boot-messaging-stomp-websocket-example"
---
+++ src/main/kotlin/messagingstompwebsocket/SpringBootMessagingStompWebsocketExampleApplication.kt
@@ -0,0 +1,11 @@
+package messagingstompwebsocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootMessagingStompWebsocketExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootMessagingStompWebsocketExampleApplication>(*args)
+}

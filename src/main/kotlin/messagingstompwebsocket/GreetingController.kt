6bb2210824e92c62f1272d13e1ab408703bf6163
koga yushi <youshe.old.pleasure@gmail.com>
2021-11-13 12:01:44 +0900

    メッセージ処理コントローラーを作成する

---
+++ src/main/kotlin/messagingstompwebsocket/GreetingController.kt
@@ -0,0 +1,17 @@
+package messagingstompwebsocket
+
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.stereotype.Controller
+import org.springframework.web.util.HtmlUtils
+
+@Controller
+class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    @Throws(Exception::class)
+    fun greeting(message: HelloMessage): Greeting {
+        Thread.sleep(1000) // simulated delay
+        return Greeting("Hello, " + HtmlUtils.htmlEscape(message.name) + "!")
+    }
+}
